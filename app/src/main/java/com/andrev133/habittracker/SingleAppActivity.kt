622c9d453da34baf7a04556e72124274f9ae6805
AnDreV133 <dmitrijkubik@gmail.com>
2025-03-10 17:24:01 +0300

    feat: добавлен активити с контейнерами фрагментов

---
+++ app/src/main/java/com/andrev133/habittracker/SingleAppActivity.kt
@@ -0,0 +1,12 @@
+package com.andrev133.habittracker;
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SingleAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_app)
+
+    }
+}
